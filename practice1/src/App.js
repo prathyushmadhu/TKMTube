5e72ac99bf753d2b2a6f544eb23ba55d43a40b4e
abhinav krishna v <96730888+abhinavkrishnav@users.noreply.github.com>
2024-02-20 18:06:05 +0530

    css2

--- practice1/src/App.js
+++ practice1/src/App.js
@@ -5,9 +5,14 @@ function App() {
   const name = "SMITH";
 
   return (
+    <div>
     <div className="App styled-div">
    <h1 className = "heading">{name}</h1>
     </div>
+    <div className = "styled-div">
+    <h1 className = "heading"> GALLERY </h1>
+   </div>
+   </div>
   );
 }
 export default App;
