4dbab74f92357c21cbdcd8d97150076d4103d9e1
abhinav krishna v <96730888+abhinavkrishnav@users.noreply.github.com>
2024-02-21 09:17:11 +0530

    components

---
+++ practice1/src/components/Child.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Child = () => {
+  return (
+    <div>Child of component 1</div>
+  )
+}
+
+export default Child
---
+++ practice1/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Child from "./Child";
+
+const Header = () => {
+  return (
+    <div>
+      Component 1
+      <Child></Child>
+    </div>
+    
+  )
+}
+
+export default Header
