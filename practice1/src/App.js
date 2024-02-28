48f22395dc12282f5c542df5a04a3249b2c6679e
abhinav krishna v <96730888+abhinavkrishnav@users.noreply.github.com>
2024-02-28 20:25:31 +0530

    frontend

---
+++ practice1/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import React from "react";
+import Header from "./components/Header";
+import Blog from "./components/Blog";
+
+
+function App() {
+
+  return (
+    <>
+   <Header />
+   <Blog/>
+   </>
+  );
+}
+export default App;
