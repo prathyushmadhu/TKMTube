529bb79004d9aa2f40cbbdc502bbdc4e7eef93b4
abhinav krishna v <96730888+abhinavkrishnav@users.noreply.github.com>
2024-04-25 17:29:18 +0530

    Package and Login

--- practice1/src/LoginPage.js
+++ practice1/src/LoginPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import { useNavigate } from 'react-router-dom';
 import './LoginPage.css';
 const LoginPage = () => {
