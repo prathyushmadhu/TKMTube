2cc8ad1ce4e0918339e9ed3b3e8a77aa3b093a8e
abhinav krishna v <96730888+abhinavkrishnav@users.noreply.github.com>
2024-05-12 10:52:44 +0530

    Animations on blog page

--- practice1/src/Navbar.js
+++ practice1/src/Navbar.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import './Navbar.css';
-
+import { motion } from "framer-motion";
 const Navbar = () => {
   return (
     <nav className="navbar">
@@ -13,15 +13,19 @@ const Navbar = () => {
 
       <div className='new-center'>
       <ul className="nav-links">
+      <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 1.0 }}>
         <li><a href="/Gallery">Gallery</a></li>
+        </motion.div>
+        <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 1.0 }}>
         <li><a href="/blog">Blog</a></li>
-        
+        </motion.div>
       </ul>
       </div>
 
       <div className='new2'>
-        
+      <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 1.0 }}>
         <div className='prolink'><a href="/Profile">Profile</a></div>
+        </motion.div>
         <div className='propik'></div>
       
       </div>
--- practice1/src/TopPosts.js
+++ practice1/src/TopPosts.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './TopPosts.css'; // Assuming you have a CSS file for styling top posts
+import { motion } from "framer-motion";
+import { useSpring, animated } from '@react-spring/web'
 
 function TopPosts() {
   const [topPosts, setTopPosts] = useState([]);
-
+  const springs = useSpring({
+    from: { y: 1000 },
+    
+    to: { y: 0 },
+    config: { duration: 2500 }
+  })
   useEffect(() => {
     axios.get('http://localhost:8080/blogs/')
       .then(response => {
@@ -17,6 +24,13 @@ function TopPosts() {
 
   return (
     <div className="top-posts">
+      <animated.div style={{
+        // width: 80,
+        // height: 80,
+        // background: '#ff6d6d',
+        // borderRadius: 8,
+        ...springs,
+      }}>
       <h1 className="top-posts-heading">Top Picks for you</h1>
       
       <ul className="top-posts-list">
@@ -25,15 +39,19 @@ function TopPosts() {
           
           <li key={post.id} className="top-posts-item">
             {/* Profile picture div */}
+            
+            <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 1.0 }}>
             <div className="profile-pic"></div>
-            <br />
+            
             {/* Blog post content */}
             <div className="post-content">
               <h2 className="top-posts-title">{post.title}</h2>
             </div>
+            </motion.div>
           </li>
         ))}
       </ul>
+      </animated.div>
     </div>
   );
 }
